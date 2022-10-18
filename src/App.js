85186d20513bbbc1f18f74b1119b1d3d96bf2efe
Levan iakobidze <levaniakobidze25@gmail.com>
2022-10-18 12:42:02 +0400

    fix router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./pages/home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 function App() {
   const [planetIndex, setPlanetIndex] = useState(0);
   const [color, setColor] = useState("#419EBB");
+
   return (
     <div className='App'>
-      <Router>
-        <Navbar
-          planetIndex={planetIndex}
-          setPlanetIndex={setPlanetIndex}
-          setColor={setColor}
-          color={color}
+      {/* <Router> */}
+      <Navbar
+        planetIndex={planetIndex}
+        setPlanetIndex={setPlanetIndex}
+        setColor={setColor}
+        color={color}
+      />
+      <Routes>
+        <Route
+          path={"/"}
+          element={
+            <Home
+              planetIndex={planetIndex}
+              setPlanetIndex={setPlanetIndex}
+              color={color}
+              setColor={setColor}
+            />
+          }
         />
-        <Routes>
-          <Route
-            path=':planet'
-            element={
-              <Home
-                planetIndex={planetIndex}
-                setPlanetIndex={setPlanetIndex}
-                color={color}
-                setColor={setColor}
-              />
-            }
-          />
-        </Routes>
-      </Router>
+        <Route
+          path={":planet" || "Mercury"}
+          element={
+            <Home
+              planetIndex={planetIndex}
+              setPlanetIndex={setPlanetIndex}
+              color={color}
+              setColor={setColor}
+            />
+          }
+        />
+      </Routes>
+      {/* </Router> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./context/appContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ContextProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ContextProvider>
 );
